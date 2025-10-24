5380d06e245d44cd782859d6f13944847941ff38
diditaditia66 <diditaditia333@gmail.com>
2025-10-24 15:55:22 +0700

    Update 3

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,9 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-export default defineConfig({ plugins: [react()] })
+
+export default defineConfig({
+  base: '/',          // penting untuk domain root
+  plugins: [react()],
+  build: { outDir: 'dist' }
+})
